f73c25299aba3e40c7fb59f7242b0465aa87eafb
Nicolas Mervaillie <nmervaillie@gmail.com>
2017-05-16 15:09:57 +0200

    Replace GraphRepository by Neo4jRepository

--- boot-1.5/src/main/java/org/neo4j/boot/test/repository/UserRepository.java
+++ boot-1.5/src/main/java/org/neo4j/boot/test/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package org.neo4j.boot.test.repository;
 
 import org.neo4j.boot.test.domain.User;
-import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface UserRepository extends GraphRepository<User> {
+public interface UserRepository extends Neo4jRepository<User, Long> {
 
 }
