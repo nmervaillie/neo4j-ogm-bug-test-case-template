fded5251404246e358040faa166d676c9e366945
Joachim Nielandt <joachim.nielandt@ugent.be>
2017-06-07 10:32:19 +0200

    Some cleanup of the Main class, this is not important.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,3 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.StatementResult;
-import org.neo4j.ogm.model.Result;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.Assert.*;
-import static org.neo4j.driver.v1.Values.parameters;
-
 /**
  * Created by jnieland on 07/06/17.
  */
